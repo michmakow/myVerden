187e3dd02c92e61f65359638408a1e58542ae344
Michał Makowski <michmakow@gmail.com>
2025-08-05 11:49:03 +0200

    MVR-1: Visual adjustements, code refactor

--- frontend/myVerden-react-app/src/components/Pages/Login/LoginPage.tsx
+++ frontend/myVerden-react-app/src/components/Pages/Login/LoginPage.tsx
@@ -1,25 +1,30 @@
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { Eye, EyeOff, Loader2Icon } from 'lucide-react';
-
-import loginPagePicture from '@/assets/login_page_picture.png';
-import loginPageSketch from '@/assets/login_page_sketch.png';
-import bigLogo from '@/assets/myVerden_logo_big.svg';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuthStore } from '@/store/AuthStore';
-
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { Eye, EyeOff, Loader2Icon } from 'lucide-react';
 import { z } from 'zod';
+
+import bigLogo from '@/assets/myVerden_logo_big.svg';
+import loginPagePicture from '@/assets/login_page_picture.png';
+import loginPageSketch from '@/assets/login_page_sketch.png';
 import {
+  Button,
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form';
+  Input,
+} from '@/components/ui/index';
+import { useAuthStore } from '@/store/AuthStore';
+
+export interface SpotlightState {
+  maskPosX: number;
+  maskPosY: number;
+  isActive: boolean;
+}
 
 const LoginPage = () => {
   const loginPagePictureAlt = 'login_page_picture';
@@ -28,8 +33,14 @@ const LoginPage = () => {
 
   const navigate = useNavigate();
   const login = useAuthStore((state) => state.login);
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [spotlightState, setSpotlightState] = useState<SpotlightState>({
+    isActive: true,
+    maskPosX: -1000,
+    maskPosY: -1000,
+  });
 
   const FormSchema = z.object({
     email: z.email({ message: 'Please enter a valid email address.' }),
@@ -46,35 +57,53 @@ const LoginPage = () => {
     },
   });
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
     setIsLoading(true);
     console.log(data);
     setTimeout(() => {
       login();
       navigate('/dashboard');
     }, 5000);
-  }
-
-  const [maskPos, setMaskPos] = useState({ x: -1000, y: -1000 });
-  const [spotlightActive, setSpotlightActive] = useState(true);
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!spotlightActive) {
-      return;
-    }
-    const rect = e.currentTarget.getBoundingClientRect();
-    setMaskPos({ x: e.clientX - rect.left, y: e.clientY - rect.top });
   };
 
-  const handleMouseLeave = () => {
-    setMaskPos({ x: -1000, y: -1000 });
+  const pageHandleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setSpotlightState((prev) => (prev.isActive ? { ...prev, maskPosX: x, maskPosY: y } : prev));
+  };
+
+  const pageHandleMouseLeave = (_: React.MouseEvent<HTMLDivElement>) => {
+    setSpotlightState((prev) => ({
+      ...prev,
+      maskPosX: -1000,
+      maskPosY: -1000,
+    }));
+  };
+
+  const formHandleMouseEnter = () => {
+    setSpotlightState((prev) => ({
+      ...prev,
+      isActive: false,
+      maskPosX: -1000,
+      maskPosY: -1000,
+    }));
+  };
+
+  const formHandleMouseLeave = () => {
+    setSpotlightState((prev) => ({
+      ...prev,
+      isActive: true,
+      maskPosX: -1000,
+      maskPosY: -1000,
+    }));
   };
 
   return (
     <div
       className='relative w-full h-screen overflow-hidden'
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
+      onMouseMove={pageHandleMouseMove}
+      onMouseLeave={pageHandleMouseLeave}
     >
       <img
         src={loginPageSketch}
@@ -86,25 +115,19 @@ const LoginPage = () => {
         alt={loginPagePictureAlt}
         className='w-full h-full absolute inset-0 object-cover pointer-events-none'
         style={{
-          WebkitMaskImage: spotlightActive
-            ? `radial-gradient(circle 100px at ${maskPos.x}px ${maskPos.y}px, transparent 0, transparent 100px, white 100px)`
+          WebkitMaskImage: spotlightState.isActive
+            ? `radial-gradient(circle 5rem at ${spotlightState.maskPosX}px ${spotlightState.maskPosY}px, transparent 0, transparent 5rem, white 5rem)`
             : 'none',
-          maskImage: spotlightActive
-            ? `radial-gradient(circle 100px at ${maskPos.x}px ${maskPos.y}px, transparent 0, transparent 100px, white 100px)`
+          maskImage: spotlightState.isActive
+            ? `radial-gradient(circle 5rem at ${spotlightState.maskPosX}px ${spotlightState.maskPosY}px, transparent 0, transparent 5rem, white 5rem)`
             : 'none',
         }}
       />
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          onMouseEnter={() => {
-            setSpotlightActive(false);
-            setMaskPos({ x: -1000, y: -1000 });
-          }}
-          onMouseLeave={() => {
-            setSpotlightActive(true);
-            setMaskPos({ x: -1000, y: -1000 });
-          }}
+          onMouseEnter={formHandleMouseEnter}
+          onMouseLeave={formHandleMouseLeave}
           className='w-2/3 space-y-6'
         >
           <div className='flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/5 w-2/11 h-4/9 primary-glass gap-5 overflow-hidden'>
@@ -124,7 +147,7 @@ const LoginPage = () => {
                         {...field}
                       />
                     </FormControl>
-                    <FormMessage />
+                    <FormMessage className='text-[var(--color-destructive-standard)]' />
                   </FormItem>
                 )}
               />
@@ -150,29 +173,37 @@ const LoginPage = () => {
                           className='absolute right-2 top-1/2 transform -translate-y-1/2'
                         >
                           {showPassword ? (
-                            <Eye className='w-5 h-5' />
+                            <Eye className='w-7 h-7' />
                           ) : (
-                            <EyeOff className='w-5 h-5' />
+                            <EyeOff className='w-7 h-7' />
                           )}
                         </button>
                       </div>
                     </FormControl>
-                    <FormMessage />
+                    <FormMessage className='text-[var(--color-destructive-standard)]' />
                   </FormItem>
                 )}
               />
             </div>
             <div className='px-5 w-fit'>
-              <a href='/reset-password' className='size-1 forgot-link'>
+              <a
+                href='/reset-password'
+                className='size-1 hover:cursor-pointer text-[var(--color-secondary)] underline underline-offset-1'
+              >
                 Forgot your password?
               </a>
             </div>
-            <div className='px-5 flex flex-row-reverse justify-center gap-4 mt-auto pb-8'>
+            <div className='px-5 w-fit'>
+              <a
+                href='/create-account'
+                className='size-1 hover:cursor-pointer text-[var(--color-gold)]'
+              >
+                Create account
+              </a>
+            </div>
+            <div className='px-5 flex flex-row-reverse justify-center  mt-auto pb-8'>
               <Button type='submit' className='primary-button p-1.5 size-1-5 text-white'>
                 {isLoading ? <Loader2Icon className='animate-spin' /> : 'LOG IN'}
-              </Button>
-              <Button type='submit' className='primary-button p-1.5 size-1-5 text-white'>
-                CREATE
               </Button>
             </div>
           </div>
